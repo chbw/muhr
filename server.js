5bea86506b81181546284e01a78fbee024b51937
Christoph B. Wurzinger <wu@htlweiz.at>
2025-05-08 22:31:22 +0200

    bumped dependencies and fixed breaking changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 
 server.listen(8080);
 
